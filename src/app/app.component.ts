828c2bc6fddcc0824a63d0aad112c9db37f5660a
Abu_Programmiy <abdukholiq23@gmail.com>
2025-01-30 18:50:06 +0500

    fixing bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template';
+
+  storedData: string | null = "endi";
+
+  constructor() {
+    if (window.Telegram) {
+      window.Telegram.WebApp.expand(); // Expands the Mini App
+    }
+  }
+
+  // Save to Cloud Storage
+  saveData() {
+    window.Telegram.WebApp.CloudStorage.setItem('user_name', 'John Doe', (err: any) => {
+      if (err) {
+        console.error('Error saving data:', err);
+      } else {
+        alert('Data saved successfully!');
+      }
+    });
+  }
+
+  // Load from Cloud Storage
+  loadData() {
+    window.Telegram.WebApp.CloudStorage.getItem('user_name', (err: any, value: string) => {
+      if (err) {
+        console.error('Error retrieving data:', err);
+      } else {
+        this.storedData = value;
+        alert(`Stored Value: ${this.storedData}`);
+      }
+    });
+  }
+
+}
+
+
+declare global {
+  interface Window {
+    Telegram: any;
+  }
 }
