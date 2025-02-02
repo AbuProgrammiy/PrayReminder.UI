7844e3927ca7d0ed19651eab8d10e8160543ace5
Abu_Programmiy <abdukholiq23@gmail.com>
2025-02-02 09:53:35 +0500

    idb starter option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { openDB } from 'idb';
+import { DatabaseService } from './service/database/database.service';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,8 @@ import { openDB } from 'idb';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private databse:DatabaseService){
+    databse.saveData("start","true")
+  }
   title = 'template';
 }
